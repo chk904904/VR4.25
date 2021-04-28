a362c464a49c92218e9020abc25fd3308e4c3f6f
yilinLucywang <72194580+yilinLucywang@users.noreply.github.com>
2021-04-27 21:46:39 -0400

    Update FaceController.cs

--- Assets/Art/Scripts/FaceController.cs
+++ Assets/Art/Scripts/FaceController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,9 @@ public class FaceController : MonoBehaviour
     public bool face_left; 
     public bool face_middle; 
     public float face_distance;
+    public GameObject leftFace; 
+    public GameObject rightFace; 
+    public GameObject middleFace; 
 
     private Vector3 left; 
     private Vector3 right; 
@@ -22,32 +25,44 @@ public class FaceController : MonoBehaviour
         face_left = false; 
         face_middle = false;
 
+        left = leftFace.transform.position; 
+        right = rightFace.transform.position; 
+        middle = middleFace.transform.position;
+
     }
 
     public void startFacePos(){
-        ori_y = GameObject.Find("Head").transform.position.y;
-        ori_x = GameObject.Find("Head").transform.position.x;
-        ori_z = GameObject.Find("Head").transform.position.z;
-        left = new Vector3(ori_x + 0.4f, ori_y + 0f, ori_z - face_distance);
-        right = new Vector3(ori_x - 0.4f, ori_y + 0f, ori_z - face_distance);
-        middle = new Vector3(ori_x + 0f, ori_y + 0f, ori_z - face_distance);
-        transform.position = right;
+        // ori_y = GameObject.Find("Head").transform.position.y;
+        // ori_x = GameObject.Find("Head").transform.position.x;
+        // ori_z = GameObject.Find("Head").transform.position.z;
+        // left = new Vector3(ori_x + 0.4f, ori_y + 0f, ori_z - face_distance);
+        // right = new Vector3(ori_x - 0.4f, ori_y + 0f, ori_z - face_distance);
+        // middle = new Vector3(ori_x + 0f, ori_y + 0f, ori_z - face_distance);
+        // transform.position = right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameObject.Find("Head").GetComponent<HeadController>().start_game){
-            Debug.Log("Face controller Hi");
-            if(face_right){
-                transform.position = right;
-            }
-            else if(face_left){
-                transform.position = left;
-            }
-            else if(face_middle){
-                transform.position = middle;
-            }
+        if(face_right){
+            leftFace.GetComponent<MeshRenderer>().enabled = false;
+            rightFace.GetComponent<MeshRenderer>().enabled = true;
+            middleFace.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else if(face_left){
+            leftFace.GetComponent<MeshRenderer>().enabled = true;
+            rightFace.GetComponent<MeshRenderer>().enabled = false;
+            middleFace.GetComponent<MeshRenderer>().enabled = false;    
+        }
+        else if(face_middle){
+            leftFace.GetComponent<MeshRenderer>().enabled = false;
+            rightFace.GetComponent<MeshRenderer>().enabled = false;
+            middleFace.GetComponent<MeshRenderer>().enabled = true;
+        }
+        else{
+            leftFace.GetComponent<MeshRenderer>().enabled = false;
+            rightFace.GetComponent<MeshRenderer>().enabled = false;
+            middleFace.GetComponent<MeshRenderer>().enabled = false;
         }
     }
 }
